2d543ea9907e3a6d04d65202f52dd36adb1f94b9
Гисматуллин Артём <DreamMagic147@yandex.ru>
2023-10-30 00:39:20 +0300

    Fix some issues

--- api_yamdb/api/routers.py
+++ api_yamdb/api/routers.py
@@ -2,9 +2,6 @@ from rest_framework.routers import DefaultRouter
 
 
 class NoPutRouter(DefaultRouter):
-    """
-    Router class that disables the PUT method.
-    """
 
     def get_method_map(self, viewset, method_map):
         bound_methods = super().get_method_map(viewset, method_map)

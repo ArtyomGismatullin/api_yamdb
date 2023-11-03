51f318e2bed6057647574a892e9b72c415419795
Гисматуллин Артём <DreamMagic147@yandex.ru>
2023-11-04 00:54:15 +0300

    Fix flake8 issues

--- api_yamdb/reviews/management/commands/csv_import.py
+++ api_yamdb/reviews/management/commands/csv_import.py
@@ -31,7 +31,7 @@ class Command(BaseCommand):
         csv_reader = reader('category.csv')
         next(csv_reader)
         for row in csv_reader:
-            category_obj = Category.objects.get_or_create(
+            _ = Category.objects.get_or_create(
                 id=row[0],
                 name=row[1],
                 slug=row[2]
@@ -41,7 +41,7 @@ class Command(BaseCommand):
         csv_reader = reader('genre.csv')
         next(csv_reader, None)
         for row in csv_reader:
-            genre_obj = Genre.objects.get_or_create(
+            _ = Genre.objects.get_or_create(
                 id=row[0],
                 name=row[1],
                 slug=row[2]
@@ -52,7 +52,7 @@ class Command(BaseCommand):
         next(csv_reader, None)
         for row in csv_reader:
             category_obj = get_object_or_404(Category, id=row[3])
-            title_obj = Title.objects.get_or_create(
+            _ = Title.objects.get_or_create(
                 id=row[0],
                 name=row[1],
                 year=row[2],
@@ -65,7 +65,7 @@ class Command(BaseCommand):
         for row in csv_reader:
             genre_obj = get_object_or_404(Genre, id=row[2])
             title_obj = get_object_or_404(Title, id=row[1])
-            genre_title_obj = GenreTitle.objects.get_or_create(
+            _ = GenreTitle.objects.get_or_create(
                 id=row[0],
                 genre=genre_obj,
                 title=title_obj
@@ -75,7 +75,7 @@ class Command(BaseCommand):
         csv_reader = reader('users.csv')
         next(csv_reader, None)
         for row in csv_reader:
-            user_obj = User.objects.get_or_create(
+            _ = User.objects.get_or_create(
                 id=row[0],
                 username=row[1],
                 email=row[2],
@@ -91,7 +91,7 @@ class Command(BaseCommand):
         for row in csv_reader:
             title_obj = get_object_or_404(Title, id=row[1])
             user_obj = get_object_or_404(User, id=row[3])
-            review_obj = Review.objects.get_or_create(
+            _ = Review.objects.get_or_create(
                 id=row[0],
                 title=title_obj,
                 text=row[2],
@@ -106,7 +106,7 @@ class Command(BaseCommand):
         for row in csv_reader:
             review_obj = get_object_or_404(Review, id=row[1])
             user_obj = get_object_or_404(User, id=row[3])
-            comment_obj = Comment.objects.get_or_create(
+            _ = Comment.objects.get_or_create(
                 id=row[0],
                 review=review_obj,
                 text=row[2],
